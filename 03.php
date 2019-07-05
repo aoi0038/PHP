ad4f7c5b51ef19dd7fb942c0e3055199b110f8c6
大宮　葵 <aoi0038@gmail.com>
2019-07-05 03:18:11 +0000

    PHP課題03

--- 03.php
+++ 03.php
@@ -4,26 +4,28 @@
 
 //1
 $name = "aoi";
-if($name = "aoi"){
-echo "私はaoiです";
+if($name == "kimura"){
+echo "私は".$name."です";
 }else{
-echo "aoiではありません";
+echo $name."ではありません";
+echo "\n";
 }
 
 
 //2
 $total = 0;
-echo $total;
 for($i=0; $i<=10000; $i++){
   $total += $i;
 }
 echo $total;
+echo "\n";
 
 
 //3
 $fruits = array("apple","orange","banana","grape","peach");
 foreach($fruits as $fruit){
-  echo $fruit . ",";
+  echo $fruit;
+  echo "\n";
 }
 
 //4【応用】
@@ -31,5 +33,8 @@ foreach($fruits as $fruit){
 for($i = 1; $i <= 100; $i++){
   if($i % 5 ==0){
     echo $i;
+    echo "\n";
   }
 }
+
+?>
